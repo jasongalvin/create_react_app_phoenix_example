216e25576e84391dafb23f7ab7066641ff850083
q1t <q1t@outlook.com>
2017-01-21 09:05:07 +0800

    Calculate fat_g for Food

--- web/views/food_view.ex
+++ web/views/food_view.ex
@@ -7,9 +7,14 @@ defmodule CreateReactAppPhoenixExample.FoodView do
 
   def render("food.json", %{food: food}) do
     %{description: food.description,
-      fat_g: food.fa_sat_g,
+      fat_g: calc_fat_g(food),
       kcal: food.kcal,
       protein_g: food.protein_g,
       carbohydrate_g: food.carbohydrate_g}
+  end
+
+  defp calc_fat_g(food) do
+    (food.fa_sat_g + food.fa_mono_g + food.fa_poly_g)
+    |> Float.round(2)
   end
 end

ac3c94e70be5584e257e8e32258dba242d3c8f2f
q1t <q1t@outlook.com>
2017-01-21 10:10:43 +0800

    Calculate fat_g for Food
    Handle cases when some of values are nil or the result of expr. is integer

--- web/views/food_view.ex
+++ web/views/food_view.ex
@@ -12,4 +12,14 @@ defmodule CreateReactAppPhoenixExample.FoodView do
       protein_g: food.protein_g,
       carbohydrate_g: food.carbohydrate_g}
   end
+
+  defp calc_fat_g(food) do
+    # Some values may be nil, TODO improve
+    fa_sat_g = food.fa_sat_g || 0
+    fa_mono_g = food.fa_mono_g || 0
+    fa_poly_g = food.fa_poly_g || 0
+    (fa_sat_g + fa_mono_g + fa_poly_g)
+    |> Kernel./(1) # always return float
+    |> Float.round(2)
+  end
 end
